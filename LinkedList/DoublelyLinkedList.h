fd1496f1bd1998c7b57c91216cbc19fe0a15c3aa
plo1 <plo1@terpmail.umd.edu>
2020-12-29 21:03:12 -0500

    Fixed additional typos

--- LinkedList/DoublelyLinkedList.h
+++ LinkedList/DoublelyLinkedList.h
@@ -21,7 +21,7 @@ DLL *dll_init();
 void dll_free(DLL *lst);
 
 DLL_Node *_dll_node_init(void *data_in, int size_in);
-void _dll_node_free(DLL *lst);
+void _dll_node_free(DLL_Node *node);
 
 int _dll_insert(DLL *lst, int pos, void *data_in, int size_in);
 void *_dll_get(DLL *lst, int pos, int *size_out);
